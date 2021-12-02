ede249fdb43e29e5612dc544610f7e063015fb66
PatrikJMelander <patrikjmelander@gmail.com>
2021-12-02 09:05:50 +0100

    created template day3 and fixed gitignore

--- src/Day3.java
+++ src/Day3.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Created by Patrik Melander
  * Date: 2021-12-02
@@ -6,4 +9,27 @@
  * Copyright: MIT
  */
 public class Day3 {
+    List<String> list = InputReader.readStringFile("test.txt");
+    public Day3() {
+        System.out.println(problem1(list));
+        System.out.println(problem2(list));
+    }
+
+    public static void main(String[] args) throws IOException {
+        long startTime = System.nanoTime();
+
+        Day3 d = new Day3();
+        System.out.printf("Execution time: %f milliseconds%n", ((double) System.nanoTime() - startTime) / 1000000);
+    }
+
+
+
+    public static int problem1(List<String> list) {
+        return 0;
+    }
+
+    public static int problem2(List<String> list) {
+
+        return 0;
+    }
 }
