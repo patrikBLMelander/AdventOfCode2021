b8006a6e483b6856399ae361fd03687c580ac6c6
PatrikJMelander <patrikjmelander@gmail.com>
2021-12-05 22:55:09 +0100

    cleanup day 4

--- src/Day4.java
+++ src/Day4.java
@@ -96,7 +96,7 @@ public class Day4 {
                                     }
 
                                 }
-                                    break;
+
                             }
                         }
                     }
