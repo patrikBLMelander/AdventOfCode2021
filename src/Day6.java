0ca56a381929238dc2e28325a23435bf42fc5b12
PatrikJMelander <patrikjmelander@gmail.com>
2021-12-06 11:42:44 +0100

    day6 complete

--- src/Day6.java
+++ src/Day6.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Patrik Melander
@@ -8,7 +11,7 @@ import java.util.List;
  * Copyright: MIT
  */
 public class Day6 {
-    List<String> list = InputReader.readStringFile("day3.txt");
+    List<String> list = InputReader.readStringFile("day6.txt");
 
     public Day6() {
         System.out.println("Answer Problem 1: " + problem1(list));
@@ -24,13 +27,66 @@ public class Day6 {
 
 
     public static int problem1(List<String> list) {
+        String[] parts = new String[999];
+        List<Integer> lanternFiches =new ArrayList<>();
+        for (String s: list) {
+           parts = s.split("," );
+        }
+        for (String s: parts) {
+            lanternFiches.add(Integer.parseInt(s));
+        }
+        int dayCounter = 0;
 
-        return 0;
+        while (dayCounter<=79) {
+            List<Integer> newDayLanternFiches = lanternFiches;
+            lanternFiches=new ArrayList<>();
+            for (int i = 0; i < newDayLanternFiches.size(); i++) {
+                int tempValue = newDayLanternFiches.get(i)-1;
+
+
+                if (newDayLanternFiches.get(i)==0){
+                    tempValue=6;
+                    lanternFiches.add(tempValue);
+                    Integer newFish = 8;
+                    lanternFiches.add(newFish);
+                }else{
+                    lanternFiches.add(tempValue);
+                }
+            }
+            dayCounter++;
+        }
+
+        return lanternFiches.size();
     }
 
-    public static int problem2(List<String> list) {
+    public static long problem2(List<String> list) {
 
+        for (String s : list) {
+            String[] startQuantity = s.split(",");
+
+            long[] lanternFiches = new long[9];
+
+            long temp;
+
+            for (int i = 0; i < startQuantity.length; i++) {
+                lanternFiches[Integer.parseInt(startQuantity[i])]++;
+            }
+
+            for (int i = 0; i < 256; i++) {
+                temp = lanternFiches[0];
+                lanternFiches[0] = lanternFiches[1];
+                lanternFiches[1] = lanternFiches[2];
+                lanternFiches[2] = lanternFiches[3];
+                lanternFiches[3] = lanternFiches[4];
+                lanternFiches[4] = lanternFiches[5];
+                lanternFiches[5] = lanternFiches[6];
+                lanternFiches[6] = temp + lanternFiches[7];
+                lanternFiches[7] = lanternFiches[8];
+                lanternFiches[8] = temp;
+            }
+
+            return lanternFiches[0] + lanternFiches[1] + lanternFiches[2] + lanternFiches[3] + lanternFiches[4] + lanternFiches[5] + lanternFiches[6] + lanternFiches[7] + lanternFiches[8];
+        }
         return 0;
     }
-
 }
